0205681ae66d9f1fc3a4da73a3f21701aa6b04c3
adarshsingh1706 <adarsh.singh2022@vitstudent.ac.in>
2024-02-21 20:03:33 +0530

    insertionOnAnyPosition

--- DSA/LinkedList/singlylinkedlist.cpp
+++ DSA/LinkedList/singlylinkedlist.cpp
@@ -23,9 +23,45 @@ void insertAtHead(Node *&head, int d)
   head = temp;
 }
 
+void insertAtTail(Node *&Tail, int d)
+{
+  // new node creation
+  Node *temp = new Node(d);
+  Tail->next = temp;
+  Tail = temp;
+}
+
+void insertAtPosition(Node *&head, Node *&Tail, int Position, int d)
+{
+
+  if (Position == 1) // incase we want to insert at pos1.
+  {
+    insertAtHead(head, d);
+    return;
+  }
+  Node *temp = head;
+  int count = 1;
+  while (count < Position - 1)
+  {
+    temp = temp->next;
+    count++;
+  }
+
+  if (temp->next == NULL)
+  { // when inserting at last
+    insertAtTail(Tail, d);
+    return;
+  }
+
+  // creating a node for d
+  Node *nodeToInsert = new Node(d);
+  nodeToInsert->next = temp->next;
+  temp->next = nodeToInsert;
+}
+
 void print(Node *&head)
 {
-  Node *temp = head;
+  Node *temp = head; // for traversing
   while (temp != NULL)
   {
     cout << temp->data << " ";
@@ -42,18 +78,40 @@ int main()
   Node *node1 = new Node(10);
   // head pointed to node1
   Node *head = node1;
+  Node *Tail = node1;
+
+  // print(head);
+  // insertAtHead(head, 12);
+  // print(head);
+  // insertAtHead(head, 13);
+  // print(head);
+  // insertAtHead(head, 14);
+  // print(head);
+  // insertAtHead(head, 15);
+  // print(head);
+  // insertAtHead(head, 16);
+  // print(head);
 
   print(head);
-  insertAtHead(head, 12);
+  insertAtTail(Tail, 12);
   print(head);
-  insertAtHead(head, 13);
+  insertAtTail(Tail, 13);
   print(head);
-  insertAtHead(head, 14);
+  insertAtTail(Tail, 14);
   print(head);
-  insertAtHead(head, 15);
+  insertAtTail(Tail, 15);
   print(head);
-  insertAtHead(head, 16);
+  insertAtTail(Tail, 16);
   print(head);
+
+  insertAtPosition(head, Tail, 3, 100);
+  print(head);
+
+  insertAtPosition(head, Tail, 1, 0);
+  print(head);
+
+  cout << "head:" << head->data << endl;
+  cout << "tail:" << Tail->data << endl;
 
   return 0;
 }
