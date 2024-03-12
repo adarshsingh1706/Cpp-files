80b4192e03da38f39f2cd8a6992d49b082580c36
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-12 19:07:30 +0530

    queue

---
+++ DSA/queue/reverseQueue.cpp
@@ -0,0 +1,27 @@
+#include<Queue>
+using namespace std;
+
+void reversequeue(std::queue<int> &q){
+            //base case
+            if(q.empty()){
+                return;
+            }
+            
+            int num = q.front();
+            q.pop();
+            reversequeue(q);
+            q.push(num);
+            
+        }
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> &q)
+    {
+      reversequeue(q);
+      return q;
+    }
+    
+};
