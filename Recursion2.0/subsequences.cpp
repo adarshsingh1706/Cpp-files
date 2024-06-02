3e336a2fd792aa580713ba0058da72cb3cecedcf
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-06-02 13:14:37 +0530

    subsequence

---
+++ Recursion2.0/subsequences.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printSubseq(int index, int n, int arr[], vector<int> &output)
+{
+  // base case
+  if (index >= n)
+  {
+    if (output.size() == 0) // for showing {} in o/p
+      cout << "{}";
+
+    for (int i = 0; i < output.size(); i++)
+    {
+      cout << output[i] << " ";
+    }
+    cout << endl;
+
+    return;
+  }
+
+  output.push_back(arr[index]); 
+  printSubseq(index + 1, n, arr, output); 
+  output.pop_back(); 
+  printSubseq(index + 1, n, arr, output);
+}
+
+int main()
+{
+  int n = 3;
+  int arr[] = {3, 2, 1};
+  vector<int> output; // for storing individual subsequences
+  printSubseq(0, 3, arr, output);
+}
