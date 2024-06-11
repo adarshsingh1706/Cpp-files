a98b2be1b2e91beaf3c98a2495e05b3533035447
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-06-11 17:51:56 +0530

    optimised algo"

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -5,13 +5,17 @@ void bubbleSort(int arr[], int n)
 {
   for (int i = 0; i < n - 1; i++)
   {
-
+      bool didSwap = false;
     for (int j = 0; j < n-i-1; j++)
     {
-      if (arr[j] > arr[j+1])
+      if (arr[j] > arr[j+1]){
         swap(arr[j], arr[j+1]);
+        didSwap=true;
     }
+    }
+    if(!didSwap) break;
   }
+ 
 }
 
 int main()
