6e8de929f47fe1a85e1d804d28ea9b739f6e1752
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-06-14 17:45:47 +0530

    deletion of head

---
+++ LinkedList/deletion.cpp
@@ -0,0 +1,64 @@
+//deleting head
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+  public:
+  int data;
+  Node* next;
+  public:
+  Node(int data1,Node*next1){ //constructor
+    data = data1;
+    next = next1;
+  }
+    public:
+    Node(int data1){
+      data = data1;
+      next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+  Node* head = new Node(arr[0]);
+  Node* mover = head;
+  for(int i=1;i<arr.size();i++){
+    Node* temp = new Node(arr[i]);
+    mover->next = temp; //chaining
+    mover=temp;
+  }
+  return head;
+}
+//printing LL
+void print(Node* head){
+  Node* temp = head;
+  while(temp!=nullptr){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+}
+
+//deleting head
+Node* deleteHead(Node* head){
+  Node* temp =head;
+  head= head->next;
+  free(temp);
+  return head;
+}
+
+
+
+int main(){
+  vector<int> arr ={17,4,5,6};
+  
+  Node* head = convertArr2LL(arr);
+  
+  print(head);
+  cout<<endl;
+  head = deleteHead(head);
+  cout<<endl; 
+  print(head);
+
+}
