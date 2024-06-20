1a581d084b34114df910b42daeca31706c30369c
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-06-20 13:22:05 +0530

    sort 0,1,2 in LL

--- LinkedList/DLL/DLL2.cpp
+++ LinkedList/DLL/DLL2.cpp
@@ -78,3 +78,84 @@
 
 //     }
 // };
+
+
+//sort 0's 1's 2's in a LL
+
+
+// Node* temp = head;
+//     int cnt0=0;
+//     int cnt1=0;
+//     int cnt2=0;
+//     while(temp!=NULL){
+//         if(temp->data == 0) cnt0++;
+//         else if(temp->data == 1) cnt1++;
+//         else{
+//             temp->data == 2;
+//             cnt2++;
+//         }
+//         temp=temp->next;
+//     }
+//      temp = head;
+//     while(temp!=NULL) {
+//         if(cnt0){
+//             temp->data = 0;
+//             cnt0--;
+//         }
+//         else if(cnt1) {
+//             temp->data = 1;
+//             cnt1--;
+//         }
+//         else{
+//             temp->data =2;
+//             cnt2--;
+//         }
+//         temp=temp->next;
+//     }
+//     return head;
+
+
+//optimised way---------------------
+
+
+// Node* sortList(Node *head){
+//     if(head==NULL || head->next == NULL) return head;
+//     Node* zeroHead = new Node(-1);
+//     Node* oneHead = new Node(-1);
+//     Node* twoHead = new Node(-1);
+
+//     Node* zero = zeroHead;
+//     Node* one = oneHead;
+//     Node* two = twoHead;
+
+//     Node*temp = head;
+
+//     while(temp!=NULL){
+//         if(temp->data==0){
+//             zero->next = temp;
+//             zero = temp;
+//         }
+//         else if(temp->data==1){
+//             one->next = temp;
+//             one = temp;
+//         }
+//         else{
+//             two->next = temp;
+//             two = temp;
+//         }
+//         temp= temp->next;
+//     }
+
+//     zero->next = (oneHead->next)? oneHead->next:twoHead->next;
+//     one->next = twoHead->next;
+//     two->next = NULL;
+
+//     Node* newHead = zeroHead->next;
+
+//     delete(zeroHead);
+//     delete(oneHead);
+//     delete(twoHead);
+
+//     return newHead;
+
+// }
