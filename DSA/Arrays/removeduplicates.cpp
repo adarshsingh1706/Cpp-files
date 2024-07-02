46026eb9b20dfb89b152e73d64b4f35e45778001
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-07-02 19:11:51 +0530

    intersection

--- DSA/Arrays/removeduplicates.cpp
+++ DSA/Arrays/removeduplicates.cpp
@@ -11,3 +11,24 @@
 //           return i+1;
 //     }
 // };
+
+
+//intersection of two arrays -II
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> ans;
+//         map<int,int> mp;
+//         for(int i=0;i<nums1.size();i++){
+//             mp[nums1[i]]++;
+//         }
+//         for(int i=0;i<nums2.size();i++){
+//             int element = nums2[i];
+//             if(mp.find(element)!=mp.end()&& mp[element]>0){
+//                 ans.push_back(element);
+//                 mp[element]--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
