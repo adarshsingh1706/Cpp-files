2431a32f90be2979f02dc3f2f6ba31cdedd59bf8
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-07-23 23:16:34 +0530

    sort ll

--- LinkedList/LL/delMiddle.cpp
+++ LinkedList/LL/delMiddle.cpp
@@ -91,3 +91,40 @@
 //         return head; 
 //     }
 // };
+
+
+//-------------------------sort a LL-------------------------
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* sortList(ListNode* head) {
+//         if(head==NULL) return NULL;
+//         if(head->next==NULL) return head;
+//         vector<int> arr;
+//         ListNode* temp = head;
+//         while(temp!=NULL){
+//             arr.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         sort(arr.begin(),arr.end());
+//         ListNode* dummyNode = new ListNode(0);
+//         ListNode* temp1 = dummyNode;
+//         for(int i=0;i<arr.size();i++){
+//             temp1->next = new ListNode(arr[i]);
+//             temp1= temp1->next;
+//         }
+//         // ListNode* newHead = dummyNode->next;
+//         // delete(dummyNode);
+//         // return newHead ;
+//         return dummyNode->next;
+//     }
+// };
