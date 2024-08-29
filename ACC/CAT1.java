1f693fce264c2c4dcc0ccf8f9bd93d82657cf154
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-08-29 18:17:03 +0530

    seg sieve & eulers phi algorithm

--- ACC/CAT1.java
+++ ACC/CAT1.java
@@ -28,3 +28,63 @@
         
 //     }
 // }
+
+
+
+//segmented sieve
+
+
+// import java.util.*;
+// public class Main{
+    
+//     static void SegSieve(int l, int h){
+//         boolean [] prime = new boolean[h+1];
+//         if(l==0){
+//             prime[0]=true;
+//             prime[1] = true;
+//         }
+//         if(l==1) prime[1] = true;
+//         for(int i =2;i*i<=h;i++){
+//             int smallestMultiple = (l/i)*i;
+//             if(smallestMultiple<l) smallestMultiple=smallestMultiple+i;
+//             for(int j=smallestMultiple;j<=h;j=j+i){
+//                 prime[j]=true;
+//             }
+//         }
+//         //print prime
+//         for(int i=l;i<=h;i++){
+//             if(prime[i]==false) System.out.print(i + " ");
+//         }
+//     }
+        
+//         public static void main(String args[]){
+//         SegSieve(0,2);
+//         }
+//     }
+
+
+
+//eulers phi algo
+
+// import java.util.*;
+// public class Main{
+//     static int phi(int n){
+//         int result = n;
+//         for(int i=2;i*i<=n;i++){
+//             if(n%i == 0){
+//                 while(n%i==0){
+//                     n=n/i;
+//                 }
+//                 result = result - result/i;
+//             }
+//         } 
+        
+//         if(n>1) result = result - result/n;
+//         return result;
+//     }
+//     public static void main(String args[]){
+//         int n= 8;
+//         System.out.print(phi(n));
+//     }
+// }
+
