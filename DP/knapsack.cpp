90b4f9838204d2e827847bf5228ab190e1a60a7e
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-09-29 13:24:31 +0530

    tabulation approach

--- DP/knapsack.cpp
+++ DP/knapsack.cpp
@@ -21,3 +21,40 @@
 //     }
 // };
 
+//tabulation-----------------------------
+
+
+// class Solution {
+    
+//   public:
+//     // Function to return max value that can be put in knapsack of capacity W.
+//     int knapSack(int W, vector<int>& wt, vector<int>& val) {
+       
+//         int n = wt.size();
+//         vector<vector<int>> dp(n+1,vector<int>(W+1));
+//         //bc
+//         for(int i=0;i<=n;i++){
+//             dp[i][0]=0;
+//         }
+//         for(int j=0;j<=W;j++){
+//             dp[0][j]=0;
+//         }
+//         //i->n,W->j
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=W;j++){
+//                 if(wt[i-1]<=j){
+//                     dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+//                 }
+//                 else{
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][W];
+//     };
+//     };
+
+
+
+
+
