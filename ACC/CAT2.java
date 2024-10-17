d2c26397f6b867ee642d1204db73a696c278f0df
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-10-17 19:39:46 +0530

    leadersinarray

--- ACC/CAT2.java
+++ ACC/CAT2.java
@@ -29,3 +29,40 @@
        
 //     }
 // }
+
+for(Map.Entry<Integer,Integer> entry:mp.entrySet()){
+
+}
+
+
+//Leaders in array ----------------------------------
+
+// import java.util.*;
+
+// public class Main {
+    
+//     public static ArrayList<Integer> leadersinArray(int []arr){
+//         ArrayList<Integer> nums = new ArrayList<>();
+//          int n = arr.length;
+//          int leader = arr[n-1];
+//          nums.add(leader);
+//          for(int i=n-2;i>=0;i--){
+//              if(arr[i]>leader){
+//                  leader = arr[i];
+//                  nums.add(leader);
+//              }
+//          }
+//          return nums;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for(int i=0;i<n;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//      ArrayList<Integer> nums = leadersinArray(arr);
+//      System.out.println(nums);
+       
+//     }
+// }
